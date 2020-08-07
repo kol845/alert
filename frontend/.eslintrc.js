f4c8a4e821de099f32388e04e5813e6bdc2b055e
kol845 <pebo-hamza@hotmail.com>
2020-08-07 12:06:06 +0200

    Folder structure and folder aliasing with babels module-resolver

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+
+  "extends": [
+    "plugin:import/errors",
+    "plugin:import/warnings"
+  ],
+  "settings": {
+    "import/resolver": {
+      "babel-module": {}
+    }
+  },
+
 };
---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+import {View,Text} from 'react-native';
+import {HelloWorld} from '@molecules';
+
+const SrcApp = () => (
+  <View>
+    <HelloWorld/>
+  </View>
+);
+
+export default SrcApp;
